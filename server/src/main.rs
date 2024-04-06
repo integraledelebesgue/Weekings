7b56729abd3a73ee1db853e05646e52066229b07
integraledelebesgue <jsmolka@student.agh.edu.pl>
2024-04-06 17:13:08 +0200

    Web server hello-world

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{self, get, App, HttpResponse, HttpServer, Responder};
+use std::io;
+use env_logger::Env;
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello")
 }
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    
+
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
+
